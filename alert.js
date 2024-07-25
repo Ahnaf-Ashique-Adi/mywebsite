0219f735a132d5bde2e195e7c3bf85f0e2fda61f
Ahnaf-Ashique-Adi <ahnafashiqueadi@gmail.com>
2024-07-25 15:21:29 +0600

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,3 @@
+
+// show alert message 
+window.alert("ADI here.Sorry for this unexpected message. I'm still learning so the website is still not optimized for mobile view. Best to use a desktop browser.THANKS ");
